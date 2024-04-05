0748035febaadd2e8102f9d7d37f7ad5091fba9a
DanEnd28 <dannyendara28@gmail.com>
2024-04-05 08:14:24 -0400

    diseño carnet

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,14 @@
 import Carnet from '/src/components/Carnet'
+import CarnetEjem from '/src/assets/img/Carnet.jpg'
 
 function App() {
 
   return (
     <>
-      <Carnet />
+      <div className='flex'>
+        <Carnet />
+        <img src={CarnetEjem} alt="Carnet" className="h-[8.3cm] w-[5.5cm] border print-hidden" />
+      </div >
     </>
   )
 }
--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -5,7 +5,38 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-carnet': "url('/src/assets/img/bg-header.svg')",
+      },
+      colors: {
+        orangecpv: {
+          100: "#ffe2cc",
+          200: "#ffc599",
+          300: "#ffa766",
+          400: "#ff8a33",
+          500: "#ff6d00",
+          600: "#cc5700",
+          700: "#994100",
+          800: "#662c00",
+          900: "#331600"
+        },
+        cyancpv: {
+          100: "#d7f2ee",
+          200: "#aee4de",
+          300: "#86d7cd",
+          400: "#5dc9bd",
+          500: "#35bcac",
+          600: "#2a968a",
+          700: "#207167",
+          800: "#154b45",
+          900: "#0b2622"
+        },
+
+        
+
+      },
+    },
   },
   plugins: [],
 }
